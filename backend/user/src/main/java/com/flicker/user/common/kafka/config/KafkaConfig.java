e15d81265c1bf7d00fa4e0aa19792d8500400938
오동규 <ehdrb700606@naver.com>
2025-02-26 11:29:22 +0900

    [refact] : Kafka분리를 위해 Configuration어노테이션 주석

--- backend/user/src/main/java/com/flicker/user/common/kafka/config/KafkaConfig.java
+++ backend/user/src/main/java/com/flicker/user/common/kafka/config/KafkaConfig.java
@@ -4,8 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties("spring.kafka")
+//@Configuration
+//@ConfigurationProperties("spring.kafka")
 @Data
 public class KafkaConfig {
 
